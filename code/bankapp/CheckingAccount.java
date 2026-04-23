3ff50eaf47c7f35799261af180b16b71cedfad51
kaylasimus <kaylasimus@gmail.com>
2026-04-22 18:23:23 -0700

    added stubs for methods

--- code/bankapp/CheckingAccount.java
+++ code/bankapp/CheckingAccount.java
@@ -2,4 +2,19 @@ package bankapp;
 
 public class CheckingAccount extends Account {
 
+	private double withdrawalLimit;
+	private double depositLimit;
+	
+	public double transferToSavings(SavingsAccount account, double amount) {
+		
+		double total = 0.0;
+		return total;
+	}
+	
+	public double transferToCredit(CreditAccount account, double amount) {
+		
+		
+		double total = 0.0;
+		return total;
+	}
 }
