f627907dfd766346a402480b88e9b3aa8e76c6d6
kaylasimus <kaylasimus@gmail.com>
2026-05-03 17:01:12 -0700

    Implemented transfering balance from checking to savings/credit

--- code/bankapp/CheckingAccount.java
+++ code/bankapp/CheckingAccount.java
@@ -1,29 +1,41 @@
 package bankapp;
 
+import java.util.*;
+
+
 public class CheckingAccount extends Account {
-
-	public CheckingAccount(double balance, ACCOUNT_STATUS status, ACCOUNT_TYPE type, Person user) {
-		super(balance, status, type, user);
-		// TODO Auto-generated constructor stub -- keeping for now for testing purposes
-	}
-
-	public CheckingAccount(double balance, ACCOUNT_STATUS status, ACCOUNT_TYPE type, Person user, String accountNumber) {
-		super(balance, status, type, user, accountNumber);
-	}
 
 	private double withdrawalLimit;
 	private double depositLimit;
+
+	public CheckingAccount(double balance, ACCOUNT_STATUS status, ACCOUNT_TYPE type, Customer user) {
+		super(balance, status, type, user, UUID.randomUUID().toString());
+		// TODO Auto-generated constructor stub -- keeping for now for testing purposes
+	}
+
+	public CheckingAccount(double balance, ACCOUNT_STATUS status, ACCOUNT_TYPE type, Customer user, String accountNumber) {
+		super(balance, status, type, user, accountNumber);
+	}
+
 	
 	public double transferToSavings(SavingsAccount account, double amount) {
+		// add amount to savings	
+		account.deposit(amount);
 		
-		double total = 0.0;
-		return total;
+		// subtract from checking
+		withdraw(amount);
+
+		// return balance of checking account
+		return getBalance();
 	}
 	
 	public double transferToCredit(CreditAccount account, double amount) {
+		// add amount to credit
+		account.deposit(amount);
 		
+		//subtract from checking
+		withdraw(amount);
 		
-		double total = 0.0;
-		return total;
+		return getBalance();
 	}
 }
