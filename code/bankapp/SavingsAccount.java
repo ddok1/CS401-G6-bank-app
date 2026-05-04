288cbf3525796aae2612c64b154b9837fc609faa
kaylasimus <kaylasimus@gmail.com>
2026-05-04 01:16:49 -0700

    Implemented applying interest and transferring from savings to credit methods

--- code/bankapp/SavingsAccount.java
+++ code/bankapp/SavingsAccount.java
@@ -5,6 +5,9 @@ import java.util.*;
 
 public class SavingsAccount extends Account {
 
+	private double interestRate = 0.04;
+	private double minimumBalance;
+	
 	public SavingsAccount(double balance, ACCOUNT_STATUS status, ACCOUNT_TYPE type, Person user) {
 		super(balance, status, type, user);
 		// TODO Auto-generated constructor stub -- keeping for now for testing purposes
@@ -15,17 +18,31 @@ public class SavingsAccount extends Account {
 	}
 	
 	public void applyInterest() {
+		// interest is applied monthly
+		// interest = (balance * 40%) / 12
 		
+		double monthlyInterest = (getBalance() * interestRate) / 12;
+		deposit(monthlyInterest);
 	}
 	
 	public double transferSavingsToChecking(CheckingAccount account, double amount) {
-		// add amount to checkings	
+		// add amount to checking
 		account.deposit(amount);
 		
 		// subtract from savings
 		withdraw(amount);
 
 		// return balance of savings account
+		return getBalance();
+	}
+	
+	public double transferSavingsToCredit(CreditAccount account, double amount) {
+		// add amount to credit (reduces credit balance)
+		account.withdraw(amount);
+				
+		//subtract from checking
+		withdraw(amount);
+		
 		return getBalance();
 	}
 }
