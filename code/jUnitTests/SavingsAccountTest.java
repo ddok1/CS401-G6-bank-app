35532f96cbd25993847c029cc7e40b3f00abfa76
kaylasimus <kaylasimus@gmail.com>
2026-05-04 01:18:29 -0700

    Written tests for transferring from savings to credit and monthly interest

--- code/jUnitTests/SavingsAccountTest.java
+++ code/jUnitTests/SavingsAccountTest.java
@@ -20,6 +20,7 @@ class SavingsAccountTest {
 	Account.ACCOUNT_STATUS status = Account.ACCOUNT_STATUS.OPEN; // status
 	Account.ACCOUNT_TYPE typeChecking = Account.ACCOUNT_TYPE.CHECKING; //type
 	Account.ACCOUNT_TYPE typeSavings = Account.ACCOUNT_TYPE.SAVINGS; //type
+	Account.ACCOUNT_TYPE typeCredit = Account.ACCOUNT_TYPE.CREDIT; //type
 	Person user = new Person(); //user
 	
 	@Test
@@ -31,6 +32,26 @@ class SavingsAccountTest {
 		
 		assertEquals(105.51, accChecking.getBalance()); // 100.50 + 5.01 = 105.51
 		assertEquals(195.00, accSavings.getBalance()); // 200.01 - 5.01 = 195
+	}
+	
+	@Test
+	void testInterestOneMonth() {
+		SavingsAccount accSavings = new SavingsAccount(100.00, status, typeSavings, user);
+		
+		accSavings.applyInterest();
+
+		assertTrue(accSavings.getBalance() >= 100.33); //(100 * 0.4)/12 = 0.3333.., 0.33.. + 100 = 100.33
+	}
+	
+	@Test
+	void testTransferSavingsToCredit() {
+		SavingsAccount accSavings = new SavingsAccount(balance2, status, typeSavings, user); // 200.01
+		CreditAccount accCredit = new CreditAccount(balance1, status, typeCredit, user); // 100.50
+		
+		accSavings.transferSavingsToCredit(accCredit, 50.0);
+		
+		assertEquals(150.01, accSavings.getBalance()); // 200.01 - 50.00 - 150.01
+		assertEquals(50.50, accCredit.getBalance()); // 100.50 - 50.00 = 50.50
 	}
 
 }
