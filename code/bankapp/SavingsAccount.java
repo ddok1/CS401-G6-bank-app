76c45114bd672e581c199bffe0063c4830df78b2
kaylasimus <kaylasimus@gmail.com>
2026-05-03 17:19:21 -0700

    Implemented method for transferring balance from savings to checking

--- code/bankapp/SavingsAccount.java
+++ code/bankapp/SavingsAccount.java
@@ -1,13 +1,31 @@
 package bankapp;
 
+import java.util.*;
+
+
 public class SavingsAccount extends Account {
 
-	public SavingsAccount(double balance, ACCOUNT_STATUS status, ACCOUNT_TYPE type, Person user) {
+	public SavingsAccount(double balance, ACCOUNT_STATUS status, ACCOUNT_TYPE type, Customer user) {
 		super(balance, status, type, user);
 		// TODO Auto-generated constructor stub -- keeping for now for testing purposes
 	}
 
-	public SavingsAccount(double balance, ACCOUNT_STATUS status, ACCOUNT_TYPE type, Person user, String accountNumber) {
+	public SavingsAccount(double balance, ACCOUNT_STATUS status, ACCOUNT_TYPE type, Customer user, String accountNumber) {
 		super(balance, status, type, user, accountNumber);
+	}
+	
+	public void applyInterest() {
+		
+	}
+	
+	public double transferSavingsToChecking(CheckingAccount account, double amount) {
+		// add amount to checkings	
+		account.deposit(amount);
+		
+		// subtract from savings
+		withdraw(amount);
+
+		// return balance of savings account
+		return getBalance();
 	}
 }
