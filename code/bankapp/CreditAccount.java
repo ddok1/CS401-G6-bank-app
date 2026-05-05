c011f689481565c79f656911cdffd55113a4ae80
kaylasimus <kaylasimus@gmail.com>
2026-05-05 11:14:42 -0700

    Implemented chargedInterest() and transferring credit balance to savings/checkings if credit balance is negative

--- code/bankapp/CreditAccount.java
+++ code/bankapp/CreditAccount.java
@@ -1,9 +1,13 @@
 package bankapp;
+import java.math.*;
+import java.time.*;
 
 public class CreditAccount extends Account {
 	
 	private double creditLimit;
-	private double interestRate = 0.2;
+	private double interestRate = 0.2; // 20%
+	private LocalDate date;
+	
 
 	public CreditAccount(double balance, ACCOUNT_STATUS status, ACCOUNT_TYPE type, Person user) {
 		super(balance, status, type, user);
@@ -21,11 +25,62 @@ public class CreditAccount extends Account {
 	}
 	
 	// increase debt
+	// initial charge, no interest yet
 	public double charge(double amount) {
 		
+		deposit(amount);
+		return getBalance();
+		
+	}
+	
+	// add to interest after a certain amount of time (end of the month)
+	public double chargeInterest() {
+		
+		// get daily rate
+		BigDecimal interestRateBD = BigDecimal.valueOf(interestRate);
+		BigDecimal dailyRate = interestRateBD.divide(BigDecimal.valueOf(365), 10, RoundingMode.HALF_UP); // 
+		
+		
+		BigDecimal balanceBD = BigDecimal.valueOf(getBalance());
+		date = LocalDate.now();
+		BigDecimal daysInMonth = BigDecimal.valueOf(date.lengthOfMonth());
+
+		
 		// multiply amount by interest rate
+		BigDecimal balanceRate = balanceBD.multiply(dailyRate); // balance * daily rate
+		BigDecimal monthlyInterest = balanceRate.multiply(daysInMonth); // balance rate * days in month
+		
+		// add to credit balance
+		deposit(monthlyInterest.doubleValue());
+		
 		// add to balance
-		double total = 0.0;
-		return total;
+		return getBalance();
+	}
+	
+	// transfer to checking or savings if credit balance is negative (bank owes user money)
+	public double transferCreditToSavings(SavingsAccount account, double amount) {
+		// check if amount is less than or equal to credit balance and is also negative
+		if (amount <= (getBalance() * -1) && getBalance() < 0.0) {
+			account.deposit(amount);
+			
+			// add to amount
+			deposit(amount);
+		}
+		
+		return getBalance();
+	}
+	
+	public double transferCreditToChecking(CheckingAccount account, double amount) {
+		
+		// make sure credit is negative 
+		if (amount <= (getBalance() * -1) && getBalance() < 0.0) {
+			// add amount to checking
+			account.deposit(amount);
+			
+			// add to credit balance
+			deposit(amount);
+
+		}
+		return getBalance();
 	}
 }
